2100618f2ad5b096ea0fc3c48d160845dc3b5828
AndyNotfound <Andy.Notfound@gmail.com>
2022-06-25 04:24:17 +0700

    fetching api

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+import {apiKey, apiHost} from './api.js';
 const userSearch = document.querySelector('#searchInput');
+let artist, isSearching;
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': apiKey,
+		'X-RapidAPI-Host': apiHost
+	}
+};
+
+function fetchingApi (){
+    fetch(`https://genius.p.rapidapi.com/search?q=${artist}`, options)
+        .then(response => response.json())
+        .then(response => destructuring(response))
+        .catch(err => console.error(err))
+}
 
 userSearch.addEventListener('keypress', (event) => {
     if (event.key === 'Enter'){ 
